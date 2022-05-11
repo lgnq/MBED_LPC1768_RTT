e103aa118c31bb2ebc6b839e2082269ed53a5c47
lgnq <dzzxzz@gmail.com>
2022-05-11 17:14:00 +0800

    all in one drv_i2c
    RT_USING_PIN, RT_USING_I2C_BITOPS

--- drivers/drv_i2c.h
+++ drivers/drv_i2c.h
@@ -11,6 +11,70 @@
 #ifndef __DRV_I2C_H__
 #define __DRV_I2C_H__
 
-void rt_hw_i2c_init(void);
+#include <rtthread.h>
+#include <rthw.h>
+#include <rtdevice.h>
+
+#include "LPC17xx.h"
+
+#ifdef RT_USING_I2C_BITOPS
+#ifdef RT_USING_PIN
+/* lpc17xx config class */
+struct lpc17xx_soft_i2c_config
+{
+    rt_uint8_t scl;
+    rt_uint8_t sda;
+    const char *bus_name;
+};
+/* lpc17xx i2c dirver class */
+
+struct lpc17xx_i2c
+{
+    struct rt_i2c_bit_ops ops;
+    struct rt_i2c_bus_device i2c2_bus;
+};
+
+/* register I2C0: SCL/P0_28 SDA/P0_27 */
+/* register I2C1: SCL/P0_01 SDA/P0_00 */
+/* register I2C2: SCL/P0_11 SDA/P0_10 */
+
+#ifdef BSP_USING_I2C0
+#define I2C0_BUS_CONFIG                     \
+    {                                       \
+        .scl = 28,                          \
+        .sda = 27,                          \
+        .bus_name = "i2c0",                 \
+    }
+#endif
+
+#ifdef BSP_USING_I2C1
+#define I2C1_BUS_CONFIG                     \
+    {                                       \
+        .scl = 1,                           \
+        .sda = 0,                           \
+        .bus_name = "i2c1",                 \
+    }
+#endif
+    
+#ifdef BSP_USING_I2C2
+#define I2C2_BUS_CONFIG                     \
+    {                                       \
+        .scl = 11,                          \
+        .sda = 10,                          \
+        .bus_name = "i2c2",                 \
+    }
+#endif
+
+#else
+#include "lpc17xx_gpio.h"
+#include "lpc17xx_pinsel.h"
+#endif
+#else
+#include "lpc17xx_i2c.h"
+#include "lpc17xx_gpio.h"
+#include "lpc17xx_pinsel.h"
+#endif
+
+int rt_hw_i2c_init(void);
 
 #endif
