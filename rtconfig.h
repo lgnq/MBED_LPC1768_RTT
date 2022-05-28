af9a69b4bd25fa3b2ceaf875ba5984c8b961b5de
lgnq <dzzxzz@gmail.com>
2022-05-28 21:07:09 +0800

    Update rtconfig.h

--- rtconfig.h
+++ rtconfig.h
@@ -68,8 +68,11 @@
 #define RT_USING_PIN
 
 #define RT_USING_I2C
-#define RT_USING_I2C_BITOPS
+#define RT_I2C_DEBUG
+// #define RT_USING_I2C_BITOPS
+#define RT_I2C_BITOPS_DEBUG
 #define BSP_USING_I2C0
+#define BSP_USING_I2C1
 #define BSP_USING_I2C2
 
 #define RT_USING_SERIAL
