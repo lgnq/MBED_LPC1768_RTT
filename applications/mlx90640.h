ce0973b9c2a76bf5e75d5bed47a4cb68c47f995a
lgnq <dzzxzz@gmail.com>
2022-05-29 12:32:29 +0800

    Update mlx90640.h
    define register address

--- applications/mlx90640.h
+++ applications/mlx90640.h
@@ -15,6 +15,10 @@
 
 #define MLX90640_I2C_ADDRESS                    0x33        // default I2C address of MLX90640 is 0x33
 
+#define STATUS_REGISTER_ADDR                    0x8000
+#define CONTROL_REGISTER_1_ADDR                 0x800D
+#define I2C_CONFIGURATION_REGISTER              0x800F
+
 /* mlx90640 device structure */
 struct mlx90640_device
 {
